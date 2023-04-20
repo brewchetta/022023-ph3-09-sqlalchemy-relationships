69b51725e993c047dff5528f7a8849dd4408b372
brewchetta <chett.tiller@gmail.com>
2023-04-20 11:52:05 -0400

    many to one one to many

--- lib/models.py
+++ lib/models.py
@@ -4,21 +4,28 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.ext.associationproxy import association_proxy
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+metadata = MetaData()
 
 Base = declarative_base(metadata=metadata)
+
+
+
+# Movie ----< Role
+# one to many relationship
+
+# FOREIGN KEY --> Role (the many)
+
+
 
 class Movie(Base):
     __tablename__ = 'movies'
 
     id = Column(Integer(), primary_key=True)
     title = Column(String())
+    year_released = Column(Integer())
 
-    roles = relationship('Role', backref=backref('movie'))
-    # roles = relationship('Role', back_populates='movie')
+    # roles = relationship('Role', backref=backref('movie'))
+    roles = relationship('Role', back_populates='movie')
 
     def __repr__(self):
         return f'<Movie(id={self.id} title={self.title} number_of_roles={len(self.roles)})>'
@@ -31,7 +38,8 @@ class Role(Base):
     actor = Column(String())
 
     movie_id = Column(Integer(), ForeignKey('movies.id'))
-    # movie = relationship('Movie', back_populates='roles')
+
+    movie = relationship('Movie', back_populates='roles')
 
     def __repr__(self):
         return f'<Role(id={self.id} character_name={self.character_name} movie_id={self.movie_id})>'
